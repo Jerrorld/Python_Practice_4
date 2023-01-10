041b71596d58300f4e66817ba1243d7f871948ff
Jerrorld <jerrorldk@gmail.com>
2023-01-09 22:09:13 -0800

    created max number in list function

---
+++ python.py
@@ -0,0 +1,11 @@
+# Write a Python function called max_num()to find the maximum of three numbers.
+def max_list(a, b, c):
+    return max([a, b, c])
+
+print(max_list(17, 38, 11))
+
+# Write a Python function called mult_list() to multiply all the numbers in a list.
+# Write a Python function called rev_string() to reverse a string.
+# Write a Python function called num_within() to check whether a number falls in a given range.
+# Write a Python function called pascal() that prints out the first n rows of Pascal's triangle.
+#
